3e6bec3466f26c6fae77c2e6924e75ad20e43d09
ajeet p <ajeetpawar07@gmail.com>
2023-04-18 12:57:22 +0530

    changed the file name

--- palindrome.js
+++ palindrome.js
@@ -7,7 +7,7 @@ for (let i = str.length - 1; i >= 0; i--) {
   bag = bag + str[i];
 }
 if (bag === str) {
-  console.log("This String is a Palindrom");
+  console.log("This String is a Palindrome");
 } else {
-  console.log("This String is not a Panlindrom");
+  console.log("This String is not a Panlindrome");
 }
